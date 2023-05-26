2c5f2c66a7ae929793833cc2bc67f1d936b307eb
Hostnomics <hostnomics@gmail.com>
2023-05-25 23:47:54 -0400

    Updated app/src/components and app/src/pages with the Thrus-May-23-2023 versions

---
+++ app/src/components/components-thurs-may-25-2023/index.js
@@ -0,0 +1,6 @@
+export { default as FormField } from './FormField';
+export { default as CustomButton } from './CustomButton';
+export { default as Navbar } from './Navbar';
+export { default as Alert } from './Alert';
+
+export { default as GetMedication } from './GetMedication';
---
+++ app/src/pages/pages-thurs-may-25-2023/index.js
@@ -0,0 +1,9 @@
+export { default as CreateScript } from './CreateScript';
+export { default as ViewScripts } from './ViewScripts';
+export { default as Home } from './Home';
+export { default as AddPatient } from './AddPatient';
+export { default as EditPatient } from './EditPatient';
+export { default as EditPharmacy } from './EditPharmacy';
+export { default as ListPharmacies } from './ListPharmacies';
+export { default as PharmacyReview } from './PharmacyReview';
+export { default as PharmacyDashboard } from './PharmacyDashboard';
