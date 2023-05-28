1961273bdfed416c51d2658ef5ac19ef99348df7
Hostnomics <hostnomics@gmail.com>
2023-05-27 23:32:41 -0400

    Updated pages, utils and constants directory with 12.1 contract address

---
+++ app/src/constants/index.jsx
@@ -0,0 +1,12 @@
+// import { useContract } from "@thirdweb-dev/react";
+
+// const { contract } = useContract("0x0967b8b29Df848E63239F5dc9314fDcff8839f7B"); //NFT Rx-10.1 - Struct, tokenURI w/ struct
+
+// export const { contract } = useContract("0x46222c35eCA09F0eDDEF9B1faA14B7a236eD0C15");  //NFT Rx-10.2 - Struct/tokenUri 721 NOT BASE! (Mint zero erro)
+// export const { contract } = useContract("0xE5960C2422B192a54988D0b7d7cD6d3f8A3a7794"); // 12.1 - Improved metadata and hasPatientRole (5/23/23)
+
+// Your NFT collection contract address"
+// export const contractAddress = "0x46222c35eCA09F0eDDEF9B1faA14B7a236eD0C15";
+
+
+export const solidityContractAddress = "0xE5960C2422B192a54988D0b7d7cD6d3f8A3a7794"; // 12.1 - Improved metadata and hasPatientRole (5/23/23)
--- app/src/utils/index.js
+++ app/src/utils/index.js
@@ -1,7 +1,21 @@
-export const convertTokenId = (txReceipt) => {
-    let formatTXReceipt = ethers.utils.formatEther(txReceipt.toString());
-    // FINAL SOLUTION IN COMMENT AT BOTTOM: https://ethereum.stackexchange.com/questions/101356/how-to-convert-bignumber-to-normal-number-using-ethers-js
-      const tokenId = Math.round(parseFloat(formatTXReceipt) * (10 ** 18));
-  
-    return tokenId;
-  };
+    export const daysLeft = (deadline) => {
+        const difference = new Date(deadline).getTime() - Date.now();
+        const remainingDays = difference / (1000 * 3600 * 24);
+    
+        return remainingDays.toFixed(0);
+    };
+
+
+    export const addyShortner = (address) => {
+        let stringAddy = String(address);
+        // String(address).substring(0, 6) + "..." + String(address).substring(address.length - 4);
+        const shortenedAddy = stringAddy.substring(0, 6) + "..." + stringAddy.substring(37)
+        return shortenedAddy;
+   }
+
+//JavaScript Mastery Kickstarter barPercentage
+    export const calculateFilledPercentage = (prescribedAmount, filledAmount) => {
+        const percentageFilled = Math.round((filledAmount * 100) / prescribedAmount);
+    
+        return percentageFilled;
+    };
