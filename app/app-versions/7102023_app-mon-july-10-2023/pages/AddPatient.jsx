707b01127e61510d9a698b0de9ac0e72f2bc1301
Hostnomics <hostnomics@gmail.com>
2023-07-15 18:29:42 -0400

    added dob and pt_physical_address fields to AddPatient.jsx page and updated its post route to the server in app/app-versions/7102023_app-mon-july-10-2023. Confirmed working.

--- app/app-versions/7102023_app-mon-july-10-2023/pages/AddPatient.jsx
+++ app/app-versions/7102023_app-mon-july-10-2023/pages/AddPatient.jsx
@@ -14,11 +14,13 @@ const AddPatient = () => {
     const [patient, setPatient] = useState({
         name: "",
         wallet_address: "",
-        email:""
+        email:"",
+        dob:"",
+        pt_physical_address:""
     });
 
     // const [name,wallet_address,email] = patient; // patient is not iterable nasty error
-    const {name,wallet_address,email} = patient; 
+    const {name,wallet_address,email,dob,pt_physical_address} = patient; 
 
     const handleChange=(e)=>{
         setPatient({...patient,[e.target.name]: e.target.value })
@@ -45,6 +47,12 @@ const AddPatient = () => {
 
     }
 
+    const handleKeyDown = (event) => {
+        if (event.keyCode === 13) {
+          event.preventDefault();
+        }
+      }
+
   return (
     <>
     <div className="container-fluid">
@@ -57,11 +65,20 @@ const AddPatient = () => {
 
         <form onSubmit={e => submitForm(e)}>
         {/* <form onSubmit={submitForm}> */}
-                    <div className="box_size">
+                    <div className="box_size_new_form">
                             <div className="row">
-                                    <div className="col-md-6">Patient Name:</div>
-                                    <div className="col-md-6">
+                                    <div className="col-md-3">Patient Name:</div>
+                                    <div className="col-md-9">
                                         <input type="text" name="name" className="form-control" value={name} onChange={(e) => handleChange(e)} />   
+                                    </div>
+                            </div>
+
+                            <div className="row">
+                                    <div className="col-md-3">DOB:</div>
+                                    <div className="col-md-4 float-left">
+                                      {/* <input type="text" name="email" className="form-control" value={email} onChange={(e) => handleChange(e)} />
+                                      <input type="date" name="dob" className="form-control" value="1969-06-09" onChange={(e) => handleChange(e)} /> */}
+                                        <input type="date" name="dob" className="form-control" value={dob} onChange={(e) => handleChange(e)} />   
                                     </div>
                             </div>
 
@@ -76,18 +93,27 @@ const AddPatient = () => {
                             </div> */}
 
                             <div className="row">
-                                    <div className="col-md-6">Wallet Address:</div>
-                                    <div className="col-md-6">
+                                    <div className="col-md-3">Wallet Address:</div>
+                                    <div className="col-md-9">
                                         <input type="text" name="wallet_address" className="form-control" value={wallet_address} onChange={(e) => handleChange(e)} />   
                                     </div>
                             </div>
 
                             <div className="row">
-                                    <div className="col-md-6">Email:</div>
-                                    <div className="col-md-6">
+                                    <div className="col-md-3">Email:</div>
+                                    <div className="col-md-9">
                                         <input type="text" name="email" className="form-control" value={email} onChange={(e) => handleChange(e)} />   
                                     </div>
                             </div>
+
+                            <div className="row">
+                                    <div className="col-md-3">Physical Address</div>
+                                    <div className="col-md-9">
+                                    <textarea name="pt_physical_address" className="form-control" value={pt_physical_address}
+                                     onChange={(e) => handleChange(e)} rows="2" onKeyDown={handleKeyDown} />                                          
+                                    </div>
+                            </div>
+
 
                         
                             <div className="row">
